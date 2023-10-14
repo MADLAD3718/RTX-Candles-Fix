4693414c86f90194af34af0e66674e146fedced4
MADLAD3718 <scheuringjack@gmail.com>
2023-10-14 15:08:57 -0400

    Execute filter chunks on world initialize

--- RTX Candles Fix Behaviours/scripts/main.js
+++ RTX Candles Fix Behaviours/scripts/main.js
@@ -1,9 +1,8 @@
 import { interactCandle, placeCandle, useItem } from "./candles";
-import { system, world } from "@minecraft/server";
 import { filterChunks } from "./chunk_filter";
+import { world } from "@minecraft/server";
 
 world.afterEvents.itemUseOn.subscribe(placeCandle);
 world.beforeEvents.itemUseOn.subscribe(useItem);
 world.afterEvents.itemStartUseOn.subscribe(interactCandle);
-
-system.run(filterChunks);
+world.afterEvents.worldInitialize.subscribe(filterChunks);
